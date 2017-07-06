2ba55cd9bc66cefdb83f1e7d01d1827fdd3614ef
serenader <xyslive@gmail.com>
2017-07-06 21:46:20 +0800

    update CheckAuth component, add redirect feature

--- src/utils/check-auth.js
+++ src/utils/check-auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react'
 import propTypes from 'prop-types'
+import qs from 'querystring'
 
 export default function checkAuth(requireAuth) {
   return function(Comp) {
@@ -9,7 +10,8 @@ export default function checkAuth(requireAuth) {
     class CheckAuth extends Component {
       static propTypes = {
         userStore: propTypes.object,
-        history: propTypes.object
+        history: propTypes.object,
+        location: propTypes.object
       }
 
       redirect = true
@@ -24,13 +26,15 @@ export default function checkAuth(requireAuth) {
 
       checkStatus() {
         const { isAuthenticated } = this.props.userStore
+        const { pathname, search } = this.props.location
         if (requireAuth) {
           if (!isAuthenticated) {
-            this.props.history.push('/admin/login')
+            this.props.history.push(`/admin/login?redirect=${pathname}${search}`)
           }
         } else {
           if (isAuthenticated) {
-            this.props.history.push('/admin')
+            const { redirect } = qs.parse(this.props.location.search.substr(1))
+            this.props.history.push(redirect || '/admin')
           }
         }
         this.redirect = (requireAuth && !isAuthenticated) || (!requireAuth && isAuthenticated)
